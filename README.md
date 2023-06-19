99546676a490561fe41c275240c76cda195951bf
Aleksandr Pokatilov <pokatilov0802@gmail.com>
2023-06-19 08:54:18 +0200

    I have no idea what I'm commiting and too lazy to check; Initial commit

--- README.md
+++ README.md
@@ -1,37 +1,21 @@
 # Mastering Zephyr Driver Development
 
-This repository contains the code for the [Mastering Zephyr Driver Development][talk]
-talk given at Zephyr Developer Summit 2022.
-
-The application is a prototype of a _smart_ lock. It allows to open/close a lock
-by both using a fingerprint reader and BLE. It is based on the official
-[example-application][example-application] and it tries to illustrate
-the most relevant concepts used when developing Zephyr drivers.
-
-[![Video](http://img.youtube.com/vi/o-f2qCd2AXo/0.jpg)](http://www.youtube.com/watch?v=o-f2qCd2AXo "Mastering Zephyr Driver Development")
-
-https://user-images.githubusercontent.com/25011557/170713288-bfdc6ebe-1bb2-4582-99e8-25f123920b6b.mp4
-
-[talk]: https://github.com/teslabs/zds-2022-drivers
-[example-application]: https://github.com/zephyrproject-rtos/example-application
+The main repository of NER-Watch. 
 
 ## Getting Started
 
-Before getting started, make sure you have a proper Zephyr development
-environment. You can follow the official
-[Zephyr Getting Started Guide](https://docs.zephyrproject.org/latest/getting_started/index.html).
+Zephyr sdk should be installed prior to the next steps.
 
 ### Initialization
 
 The first step is to initialize the workspace folder where the
-`zds-2022-drivers-app` and needed Zephyr modules will be cloned. You can do
-that by running:
+`ner-watch` and needed Zephyr modules will be cloned.
 
 ```shell
-# initialize workspace for the zds-2022-drivers-app (main branch)
-west init -m https://github.com/teslabs/zds-2022-drivers-app --mr main zds-2022-drivers-app
+# initialize workspace for the ner-watch
+west init -m https://github.com/ --mr main ner-watch
 # update Zephyr modules
-cd zds-2022-drivers-app
+cd ner-watch
 west update
 ```
 
@@ -40,13 +24,8 @@ west update
 The application can be built by running:
 
 ```shell
-west build -b nrf52840dk_nrf52840 -s app
+west build -b esp32 -s app --sysbuild
 ```
-
-Note that any other board may be used if an appropriate overlay is provided (see
-`app/boards`).
-
-Once you have built the application you can flash it by running:
 
 ```shell
 west flash
--- app/src/main.c
+++ app/src/main.c
@@ -3,227 +3,234 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/gatt.h>
-#include <zephyr/bluetooth/uuid.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/sensor.h>
+#include <stdio.h>
+#include <string.h>
+
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/smf.h>
+#include <zephyr/drivers/display.h>
 
-#include <app/drivers/lock.h>
-#include <app/drivers/sensor/jm101.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(app, CONFIG_LOG_DEFAULT_LEVEL);
 
-static const struct device *fpreader = DEVICE_DT_GET(DT_NODELABEL(fpreader));
-static const struct device *lock = DEVICE_DT_GET(DT_NODELABEL(lock));
+#include <app/drivers/gesture.h>
+#include <lvgl.h>
 
-/** @brief Lock Service UUID (equal to Nordic LBS). */
-#define BT_UUID_LOCK_VAL                                                       \
-	BT_UUID_128_ENCODE(0x00001523, 0x1212, 0xefde, 0x1523, 0x785feabcd123)
-#define BT_UUID_LOCK BT_UUID_DECLARE_128(BT_UUID_LOCK_VAL)
+#include <app/list.h>
 
-/** @brief Lock action (open/close) characteristic UUID. */
-#define BT_UUID_LOCK_ACTION_VAL                                                \
-	BT_UUID_128_ENCODE(0x00001525, 0x1212, 0xefde, 0x1523, 0x785feabcd123)
-#define BT_UUID_LOCK_ACTION BT_UUID_DECLARE_128(BT_UUID_LOCK_ACTION_VAL)
+#define EVENT_GESTURE_UP    BIT(0)
+#define EVENT_GESTURE_DOWN  BIT(1)
+#define EVENT_GESTURE_LEFT  BIT(2)
+#define EVENT_GESTURE_RIGHT BIT(3)
+#define EVENT_GESTURE_TO    BIT(4)
+#define EVENT_GESTURE_FROM  BIT(5)
 
-/** @brief Bluetooth advertisement data */
-static const struct bt_data ad[] = {
-	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME,
-		sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+#define EVENT_GESTURES      EVENT_GESTURE_UP    | EVENT_GESTURE_DOWN | \
+                            EVENT_GESTURE_RIGHT | EVENT_GESTURE_LEFT |  \
+                            EVENT_GESTURE_TO    | EVENT_GESTURE_FROM
+
+#define EVENT_BUTTON_PRESS BIT(6)
+
+#define EVENT_ALL EVENT_GESTURES | EVENT_BUTTON_PRESS
+
+static const struct smf_state app_states[];
+enum watch_state { SLEEP, APP, SELECT };
+
+struct s_object {
+    struct smf_ctx ctx;
+
+    struct k_event smf_event;
+    int32_t events;
+
+    size_t app_index;
+    struct app * app;
+} s_obj;
+
+static void sleep_entry(void *o) {
+    /* display_ */
+}
+
+static void sleep_exit(void *o) {
+}
+
+static void app_entry(void *o) {
+    struct s_object *obj = (struct s_object *) o;
+    
+    printk("App entry %d\n", obj->app_index);
+    obj->app = &app_list[obj->app_index];
+    /* printk("App name: %s\n", app_list[app_ */
+    /* printk("0x%p\n", obj->app); */
+    /* printk("%s\n", obj->app->name); */
+
+    k_thread_resume(obj->app->thread);
+}
+
+static void app_run(void *o) {
+    int32_t events = ((struct s_object *) o)->events;
+
+    if (events & EVENT_BUTTON_PRESS) {
+        smf_set_state(SMF_CTX(&s_obj), &app_states[SLEEP]);
+    } else if (events & EVENT_GESTURE_TO) {
+        smf_set_state(SMF_CTX(&s_obj), &app_states[SELECT]);
+    } else if (events & EVENT_GESTURE_DOWN) {
+        printk("App scroll down\n");
+    } else {
+        
+    }
+}
+
+static void app_exit(void *o) {
+    struct s_object *obj = (struct s_object *) o;
+
+    k_thread_suspend(obj->app->thread);
+}
+
+static void select_run(void *o) {
+    struct s_object *obj = (struct s_object *) o;
+    int32_t events = obj->events;
+
+    if (events & EVENT_BUTTON_PRESS) {
+        smf_set_state(SMF_CTX(&s_obj), &app_states[SLEEP]);
+    } else if (events & EVENT_GESTURE_FROM) {
+        smf_set_state(SMF_CTX(&s_obj), &app_states[APP]);
+    } else if (events & EVENT_GESTURE_LEFT) {
+        printk("Selector swipe left\n");
+        obj->app_index++;
+        obj->app_index %= app_count;
+    } else if (events & EVENT_GESTURE_RIGHT) {
+        printk("Selector swipe right\n");
+        obj->app_index--;
+        obj->app_index %= app_count;
+    } else {
+    }    
+}
+
+static const struct smf_state app_states[] = {
+    [SLEEP]  = SMF_CREATE_STATE(sleep_entry, NULL, sleep_exit, NULL),
+    [APP]    = SMF_CREATE_STATE(app_entry, app_run, app_exit, NULL),
+    [SELECT] = SMF_CREATE_STATE(NULL, select_run, NULL, NULL),
 };
 
-/** @brief Bluetooth service discovery data */
-static const struct bt_data sd[] = {
-	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_LOCK_VAL),
-};
-
-/*******************************************************************************
- * Bluetooth services handling
- ******************************************************************************/
-
-static ssize_t write_lock(struct bt_conn *conn, const struct bt_gatt_attr *attr,
-			  const void *buf, uint16_t len, uint16_t offset,
-			  uint8_t flags)
-{
-	uint8_t val;
-
-	if (len != 1U) {
-		return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN);
+int main(void) {
+	const struct device *display_dev;
+    
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		LOG_ERR("Display is not ready");
+		return 0;
 	}
 
-	if (offset != 0) {
-		return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
-	}
+    LOG_INF("app list allocated: %s", app_list == NULL ? "false" : "true");
+    LOG_INF("app list size: %zu", app_count);
 
-	val = *((uint8_t *)buf);
+    /* struct app clock; */
+    size_t clock_app_index = 0;
 
-	if (val == 0x00U) {
-		lock_close(lock);
-	} else if (val == 0x01U) {
-		lock_open(lock);
-	} else {
-		return BT_GATT_ERR(BT_ATT_ERR_VALUE_NOT_ALLOWED);
-	}
+    for (size_t i = 0; i < app_count; i++) {
+        if (strncmp(app_list[i].name, "clock", 5) == 0) {
+            printk("Clock app at index %zu\n", i);
+            clock_app_index = i;
+            /* clock = app_list[i]; */
+        }
 
-	return len;
+        k_thread_start(app_list[i].thread);
+        k_thread_suspend(app_list[i].thread);
+    }
+    
+    s_obj.app_index = clock_app_index;
+
+    int32_t ret;
+    k_event_init(&s_obj.smf_event);
+
+    smf_set_initial(SMF_CTX(&s_obj), &app_states[APP]);
+
+	display_blanking_off(display_dev);
+    
+    while (1) {
+        s_obj.events = k_event_wait(&s_obj.smf_event, EVENT_GESTURES, true, K_FOREVER);
+        if (s_obj.events)
+            printk("Events: 0x%x\n", s_obj.events);
+
+        ret = smf_run_state(SMF_CTX(&s_obj));
+        if (ret) {
+            LOG_ERR("state machine results in error: %d", ret);
+            break;
+        }
+
+        /* k_sleep(K_MSEC(100)); */
+    }
+
+    /* k_thread_start(clock.thread); */
+
+    return 0;
+    /* lv_scr_load(clock.screen); */
+
+	/* lv_task_handler(); */
 }
 
-/* Lock Service Declaration */
-BT_GATT_SERVICE_DEFINE(lock_svc, BT_GATT_PRIMARY_SERVICE(BT_UUID_LOCK),
-		       BT_GATT_CHARACTERISTIC(BT_UUID_LOCK_ACTION,
-					      BT_GATT_CHRC_WRITE,
-					      BT_GATT_PERM_WRITE, NULL,
-					      write_lock, NULL), );
-
-static void connected(struct bt_conn *conn, uint8_t err)
-{
-	if (err != 0) {
-		printk("Connection failed (err %u)\n", err);
-		return;
-	}
-
-	printk("Connected\n");
+void gesture_handler(const struct device *dev) {
+    enum gesture_type gesture = 0;
+    
+    gesture_get(dev, &gesture);
+    switch (gesture) {
+    case GESTURE_UP:
+        printk("Up\n");
+        k_event_post(&s_obj.smf_event, EVENT_GESTURE_UP);
+        break;
+    case GESTURE_DOWN:
+        printk("Down\n");        
+        k_event_post(&s_obj.smf_event, EVENT_GESTURE_DOWN);
+        break;
+    case GESTURE_LEFT:
+        printk("Left\n");
+        k_event_post(&s_obj.smf_event, EVENT_GESTURE_LEFT);
+        break;
+    case GESTURE_RIGHT:
+        printk("Right\n");
+        k_event_post(&s_obj.smf_event, EVENT_GESTURE_RIGHT);
+        break;
+    case GESTURE_FORWARD:
+        printk("To\n");
+        k_event_post(&s_obj.smf_event, EVENT_GESTURE_TO);
+        break;
+    case GESTURE_BACKWARD:
+        printk("From\n");
+        k_event_post(&s_obj.smf_event, EVENT_GESTURE_FROM);
+        break;
+    case GESTURE_NONE:
+        break;
+    }
 }
 
-static void disconnected(struct bt_conn *conn, uint8_t reason)
+K_THREAD_STACK_DEFINE(gesture_stack_area, 1024);
+static struct k_thread gesture_data;
+
+/* threadA is a static thread that is spawned automatically */
+
+void gesture_reader(void *dummy1, void *dummy2, void *dummy3)
 {
-	printk("Disconnected (reason %u)\n", reason);
+	ARG_UNUSED(dummy1);
+	ARG_UNUSED(dummy2);
+	ARG_UNUSED(dummy3);
+
+    const struct device *gesture_dev;
+
+	gesture_dev = DEVICE_DT_GET(DT_CHOSEN(nerw_gesture));
+	if (!device_is_ready(gesture_dev)) {
+		LOG_ERR("Gesture sensor is not ready");
+		return 0;
+	}
+
+	while (1)
+	{
+		gesture_handler(gesture_dev);
+		k_msleep(100);
+	}
+
 }
 
-BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected = connected,
-	.disconnected = disconnected,
-};
-
-/*******************************************************************************
- * Fingerprint handling
- ******************************************************************************/
-
-static int enroll(void)
-{
-	const struct gpio_dt_spec btn = GPIO_DT_SPEC_GET(DT_ALIAS(sw0), gpios);
-	int ret;
-
-	if (!device_is_ready(btn.port)) {
-		return -ENODEV;
-	}
-
-	ret = gpio_pin_configure_dt(&btn, GPIO_INPUT);
-	if (ret < 0) {
-		return ret;
-	}
-
-	if (gpio_pin_get_dt(&btn) > 0) {
-		struct sensor_value val = {
-			.val1 = 1U, /* store as fingerprint ID #1 */
-			.val2 = 2U, /* number of readings */
-		};
-
-		/* empty database first */
-		ret = sensor_attr_set(fpreader, JM101_CHAN_FINGERPRINT,
-				      JM101_ATTR_EMPTYDB, NULL);
-		if (ret < 0) {
-			printk("Could not empty database\n");
-			return ret;
-		}
-
-		printk("Place finger on sensor for enrollment\n");
-		ret = sensor_attr_set(fpreader, JM101_CHAN_FINGERPRINT,
-				      JM101_ATTR_ENROLL, &val);
-		if (ret < 0) {
-			printk("Enrollment failed\n");
-			return ret;
-		}
-
-		printk("Enrollment finished successfully!\n");
-	}
-
-	return 0;
-}
-
-void fp_trig_handler(const struct device *dev,
-		     const struct sensor_trigger *trigger)
-{
-	int ret;
-	struct sensor_value val;
-
-	ARG_UNUSED(trigger);
-
-	printk("Finger detected, checking...\n");
-
-	ret = sensor_sample_fetch_chan(dev, JM101_CHAN_FINGERPRINT);
-	if (ret < 0) {
-		printk("Verification failed\n");
-		return;
-	}
-
-	(void)sensor_channel_get(dev, JM101_CHAN_FINGERPRINT, &val);
-
-	printk("Verified fingerprint #%d, score: %d\n", val.val1, val.val2);
-
-	printk("Opening lock...\n");
-	ret = lock_open(lock);
-	if (ret < 0) {
-		printk("Could not open lock\n");
-		return;
-	}
-
-	printk("Will close lock in 5 seconds...\n");
-	k_sleep(K_SECONDS(5));
-
-	ret = lock_close(lock);
-	if (ret < 0) {
-		printk("Could not close\n");
-		return;
-	}
-
-	printk("Closed!\n");
-}
-
-/*******************************************************************************
- * Entry point
- ******************************************************************************/
-
-void main(void)
-{
-	int ret;
-	const struct sensor_trigger trig = {
-		.type = SENSOR_TRIG_TAP,
-		.chan = JM101_CHAN_FINGERPRINT,
-	};
-
-	if (!device_is_ready(fpreader)) {
-		printk("Fingerprint reader not ready");
-		return;
-	}
-
-	if (!device_is_ready(lock)) {
-		printk("Lock not ready");
-		return;
-	}
-
-	/* run enroll (will be skipped if sw0 is not pressed) */
-	(void)enroll();
-
-	/* configure fingerprint trigger */
-	ret = sensor_trigger_set(fpreader, &trig, fp_trig_handler);
-	if (ret < 0) {
-		printk("Could not configure trigger");
-	}
-
-	/* enable Bluetooth and start advertising */
-	ret = bt_enable(NULL);
-	if (ret < 0) {
-		printk("Bluetooth init failed (ret %d)\n", ret);
-		return;
-	}
-
-	ret = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd,
-			      ARRAY_SIZE(sd));
-	if (ret < 0) {
-		printk("Advertising failed to start (%d)", ret);
-		return;
-	}
-}
+K_THREAD_DEFINE(gesture_tid, 1024,
+                gesture_reader, NULL, NULL, NULL,
+                7, 0, 0);
